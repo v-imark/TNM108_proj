5a29c0ec470e92a81fb8427448d966d182a5364a
vicim994 <72468847+vicim994@users.noreply.github.com>
2022-12-02 10:29:54 +0100

    Svar i guit på prediction

--- gui.py
+++ gui.py
@@ -9,26 +9,35 @@ from initDatabase import app_stopwords
 class Gui:
     def __init__(self, master, vzer: CountVectorizer, tfidf, clf, ):
         self.master = master
+        self.prediction = tk.StringVar()
         self.frame_title = tk.Frame(master)
-        self.frame_body = tk.Frame(master)
+        self.frame_body1 = tk.Frame(master)
+        self.frame_body2 = tk.Frame(master)
 
         self.master.title("Real or Fake News?")
 
         self.label_header = tk.Label(
             master=self.frame_title,
             text="Real or FAKE news?",
-            width=100,
+            font=("Arial", 25),
         )
         self.label_header.pack()
 
+        self.answer_text = tk.Label(
+            master=self.frame_body1,
+            textvariable=self.prediction,
+            font='Arial',
+        )
+        self.answer_text.pack()
+
         self.entry_text = tk.Text(
-            master=self.frame_body,
+            master=self.frame_body2,
             height=25,
         )
-        self.entry_text.pack(pady=10)
+        self.entry_text.pack(pady=10, padx=10)
 
         self.button = tk.Button(
-            master=self.frame_body,
+            master=self.frame_body2,
             text='Predict',
             width=25,
             height=5,
@@ -37,7 +46,8 @@ class Gui:
         self.button.pack()
 
         self.frame_title.pack()
-        self.frame_body.pack(pady=10)
+        self.frame_body1.pack(pady=10)
+        self.frame_body2.pack(pady=10)
 
     def buttonpress(self, vzer: CountVectorizer, tfidf, clf):
         text = self.entry_text.get("1.0", 'end-1c')
@@ -45,5 +55,9 @@ class Gui:
         prediction = predictor(vzer, tfidf, clf, text)
         if prediction[0] == 0:
             print('Fake news')
+            self.prediction.set('This is FAKE news')
+            self.answer_text.config(fg='red')
         else:
             print('Real News')
+            self.prediction.set('This is REAL news')
+            self.answer_text.config(fg='green')
